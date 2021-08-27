e7ec738f45274042111119f0cd36c7f48342bd39
mkzaman <mkzamanalive@gmail.com>
2021-08-27 13:40:44 +0600

    updated client to ensure receiving message from right server

--- Devops for Developers/Network fundamentals/UDP Client Server Python Program/client.py
+++ Devops for Developers/Network fundamentals/UDP Client Server Python Program/client.py
@@ -3,11 +3,19 @@ import socket
 MAX_SIZE_BYTES = 65535
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-message = input("Input lowercase message")
-data = message.encode('ascii')
-s.sendto(data, ('127.0.0.1', 3000))
-print('The OS assigned the address {} to me'.format(s.getsockname()))
-data, address = s.recvfrom(MAX_SIZE_BYTES)
-text = data.decode('ascii')
-print('The server {} replied with {!r}'.format(address, text))
+hosts = []
+while True:
+    port = 3000
+    host = input('Input host address:' )
+    hosts.append((host,port))
+    message = input("Input lowercase message to send to server:" )
+    data = message.encode('ascii')
+    s.sendto(data, (host, port))
+    print('The OS assigned the address {} to me'.format(s.getsockname()))
+    data, address = s.recvfrom(MAX_SIZE_BYTES)
+    text = data.decode('ascii')
+    if(address in hosts):
+        print('The server {} replied with {!r}'.format(address, text))
+        hosts.remove(address)
+    else:
+        print('message {!r} from unexpected host {}!'.format(text, address))
